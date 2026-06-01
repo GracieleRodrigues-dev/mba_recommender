16ea11aca1493d6eaa9c7bea194eab21fdfa9cba
GracieleRodrigues-dev <graciele_rodrigues@outlook.com.br>
2026-06-01 10:02:52 -0300

    ajuste de arquivo base para deploy no streamlit

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 # General configuration for paths and parameters 
-DATA_PATH = "data/"
-STUDENT_PROFILE_FILE = DATA_PATH + "perfil_academicos.csv" 
-ENROLLMENT_FILE = DATA_PATH + "matriculas_udesc.csv" 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, '..', 'data')
+STUDENT_PROFILE_FILE = os.path.join(DATA_PATH, "perfil_academicos.csv")
+ENROLLMENT_FILE = os.path.join(DATA_PATH, "matriculas_udesc.csv")
 
 MIN_SUPPORT = 0.005
 MIN_CONFIDENCE = 0.10
